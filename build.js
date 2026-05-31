06c732cb68bcdf109106b5598ef8eaa9159025e0
buqizixv <wangpanzun@qq.com>
2026-05-31 22:55:17 +0800

    Add article: 1X NEO humanoid robot mass production 2026

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ const path = require('path');
 
 const BASE_URL = 'https://robotd.net';
 const SITE_NAME = 'Robot D';
-const V = '19';
+const V = '20';
 
 function escapeHtml(str) {
   return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
